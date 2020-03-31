4f52d43ee16cbad22708f43e46b2b3dbf6618a82
rmlsuperville <raphee54@hotmail.com>
2020-03-31 13:35:22 -0300

    added mysql db config info

--- config/database.js
+++ config/database.js
@@ -1,18 +1,12 @@
 var Sequelize = require("sequelize");
 const config = require('config');
 
-var mode;
 
-mode = 'dev';
+//Actual DB Credentials in the default.json file
 
-if(mode == 'dev'){
-  module.exports = new Sequelize(config.get('DB_URI'));
-  /*module.exports = new Sequelize('system_db', 'dev_acc', 'password123', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });*/
-}else{
-  //module.exports = new Sequelize('postgres://fqknkkeohildyw:9f4233bcbcee9032aadc026fd149202dd3b5a62f3e16a7d45f754972a1b4c7be@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d25ncrs4mufb9r');
-}
+module.exports = new Sequelize(config.get('DB_NAME'), config.get('DB_USER'), config.get('DB_PASSWORD'), {
+    host: config.get('DB_HOST'),
+    dialect: 'mysql'
+});
 
 
